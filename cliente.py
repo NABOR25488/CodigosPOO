c85458260207fb327ae63db93c0863287f7e03b5
NABOR25488 <nabormarques9@gmail.com>
2026-09-21 20:07:59 -0400

    Criando Classe Criente

--- cliente.py
+++ cliente.py
@@ -1,9 +1,29 @@
+class Cliente:
+    #construtor + atributos
 
+    def __init__(self, nome, cpf, tel, email, endereco):
+        self.nome=nome
+        self.cpf=cpf
+        self._Telefone=tel
+        self.email=email
+        self.endereco=endereco
 
-#construtor + atributos
+    # encapsulamento (analizar se precisa)
 
+    def getTelefone(self):
+        return self._Telefone
 
-# encapsulamento (analizar se precisa)
-
+    def setTelefone(self, tel):
+        self._Telefone=tel
 
 #metodos - acoes#
+
+    def imprimirficha(self):
+        print(f"\n------------------ CLIENTE--------------------"
+              f"\nNOME: {self.nome} "
+              f"\nENDERECO: {self.endereco}"
+              f"\nCPF: {self.cpf}"
+              f"\nEMAIL: {self.email}"
+              f"\nTELEFONE: {self._Telefone}"
+              f"\n----------------------------------------------"
+              )
--- main.py
+++ main.py
@@ -1,28 +1,33 @@
 from Pedido import Pedido
+from cliente import Cliente
 
 #criar um objeto - representar um elemento - dar valores 
-novoPedido = Pedido(1, "14/09/2026", "21:10", "Rafael",
-                     ["X-Salada", "X-bacon"], "Pix")
+#novoPedido = Pedido(1, "14/09/2026", "21:10", "Rafael",
+#                     ["X-Salada", "X-bacon"], "Pix")
 
 ###### o que eu posso fazer com o Objeto? ####
 ##acessar um atributo
-print(novoPedido.cliente)
-print(novoPedido.status)
+#print(novoPedido.cliente)
+#print(novoPedido.status)
 
 ##alterar os dados de um atributo
-novoPedido.cliente="Rafael Martins"
-print(novoPedido.cliente)
+#novoPedido.cliente="Rafael Martins"
+#print(novoPedido.cliente)
 
 ##chamando os metodos
-novoPedido.imprimir()
-novoPedido.atualizar_pedido("Em preparação")
+#novoPedido.imprimir()
+#novoPedido.atualizar_pedido("Em preparação")
 
 #acessar o id - private
 #novoPedido.__num=2 #ERRO PO SER PRIVADO
 #print(novoPedido.__num) #acessar - ERRO PO SER PRIVADO
-print(novoPedido.getNum())
-novoPedido.setNum(2)
-print(novoPedido.getNum())
+#print(novoPedido.getNum())
+#novoPedido.setNum(2)
+#print(novoPedido.getNum())
 
-novoPedido.setIten("X-Calabresa")
-novoPedido.imprimir()
+#novoPedido.setIten("X-Calabresa")
+#novoPedido.imprimir()#
+
+novoCliente = Cliente(endereco="rua vital brasil", nome="sla", cpf="38483", email= "jefjefnje@gmail.com",  tel="93489589" )
+novoCliente.imprimirficha()
+
